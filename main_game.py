95daf02eb21fef478fe2390adead6e9f7ce02c8c
RegevO <regevosher@gmail.com>
2025-11-11 19:55:50 +0200

    adding hello print

--- main_game.py
+++ main_game.py
@@ -1,1 +1,32 @@
-print("Welcome to Hangman game!!")
+import random
+
+
+
+list_of_words = [
+    "banana",
+    "monkey",
+    "zoom",
+    "cat",
+    "dog",
+    "avocado",
+    "epic"
+]
+
+def choose_a_random_word(word_list: list):
+    len_of_list = len(word_list)
+    random_index = random.randint(a=0, b=len_of_list - 1)
+    return word_list[random_index]
+
+
+def print_hidden_word(word):
+    word_len = len(word)
+    print('_ ' * word_len)
+
+
+if __name__ == '__main__':
+    print("Welcome to Hangman game!!")
+    word = choose_a_random_word(list_of_words)
+    print(word)
+    print_hidden_word(word)
+
+
